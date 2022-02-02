e04efc5e0a631852ac57b1c5ead462f910caad17
Arsh Patel <97227357+Arsh510@users.noreply.github.com>
2022-02-02 15:55:11 +0530

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,28 @@
+from cProfile import label
+from pyexpat import features
+import numpy as np
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+
+iris = datasets.load_iris()
+
+print(iris.DESCR)
+# {
+#     - sepal length in cm
+#     - sepal width in cm
+#     - petal length in cm
+#     - petal width in cm
+    #   - class:
+    #           - Iris-Setosa[0]
+    #           - Iris-Versicolour[1]
+    #           - Iris-Virginica[2]
+# }
+
+# features = iris.data
+# label = iris.target
+
+# clf = KNeighborsClassifier()
+# clf.fit(features,label)
+
+# predict = clf.predict([[2,4,5,8]])
+# print(predict)
